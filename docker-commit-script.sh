0fc6ce9b9cac0a4732c4e79f0aafe7fb51119874
Ajeet Singh Raina, Docker Captain, ARM Innovator <ajeetraina@gmail.com>
2022-08-25 13:51:35 +0530

    Update docker-commit-script.sh

--- docker-commit-script.sh
+++ docker-commit-script.sh
@@ -4,9 +4,9 @@ SERVICE=${1}
 IMAGE=${2}
 
 # Commit changes to image
-CONTAINER_ID=$(docker ps -aqf “name=${SERVICE}”)
+CONTAINER_ID=$(docker ps -aqf "name=${SERVICE}")
 
-if [ ! -z “$CONTAINER_ID”]; then
+if [ ! -z "$CONTAINER_ID" ]; then
 	Echo “--- Committing changes from $SERVICE to $IMAGE --- ”
 	docker commit $CONTAINER_ID $IMAGE
 fi
